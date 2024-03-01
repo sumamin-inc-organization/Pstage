57b83b5e1f6f5adb6a9b68b28d837ca519ae0279
kumagawa shoma <s.kumagawa@sumamin.com>
2024-03-01 14:28:22 +0900

     チェックシート修正

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import "./css/reset.css";
-import "./css/top.css";
-import "./css/kv.css";
-import "./css/feature.css";
-import "./css/decision.css";
-import "./css/flow.css";
-import "./css/price.css";
-import "./css/video.css";
-import "./css/voice.css";
+import "./css/reset.css"
+import "./css/top.css"
+import "./css/kv.css"
+import "./css/feature.css"
+import "./css/decision.css"
+import "./css/flow.css"
+import "./css/price.css"
+import "./css/video.css"
+import "./css/voice.css"
 import "./css/faq.css";
 
 
