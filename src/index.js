8f5f48bc578ecdf7d9e767b923c72f151a560b7c
kumagawa shoma <s.kumagawa@sumamin.com>
2024-03-06 16:17:09 +0900

    アニメーションタイミング修正

--- src/index.js
+++ src/index.js
@@ -33,121 +33,227 @@ $(function() {
 
 /*  出現アニメーション */
 document.addEventListener("DOMContentLoaded", function() {
-    /* kvセクション */
-    // let kvText01 = gsap.timeline({
-    //     scrollTrigger: {
-    //         trigger: ".kv_text01",
-    //         start: "-=100px",
-    //         scrub: false,
-    //     },
-    // });
-    // kvText01
-    // .from(".kv_text01", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"}, "+=0.4");
-
-    /* featureセクション */
-    let speech = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".feature_headline",
-            start: "top",
-            scrub: false,
-        },
-    });
-    speech
-    .from(".feature_head .speech", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"}, "+=0.4");
-
-    /* flowセクション */
-    let flow_upper_title = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".flow_headline",
-            start: "top",
-            scrub: false,
-        },
-    });
-    flow_upper_title
-    .from(".flow_upper_title", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"}, "+=0.4");
-
-    let  flowSupport = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".flow_support",
-            start: "-=100px",
-            scrub: false,
-        },
-    });
-
-    flowSupport
-    .from(".kirakira_01", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"})
-    .from(".kirakira_02", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"}, "-=0.4");
-
-    /* priceセクション */
-    let price_upper_arraow = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".price_upper_arraow",
-            start: "-=500px",
-            scrub: false,
-        },
-    });
-    price_upper_arraow
-    .from(".price_upper_arraow", { y: -10, duration: 0.5, ease: "Power4.easeOut"}, "+=0.4");
-
-    let hukidashi = CSSRulePlugin.getRule(".subsc_course::after");
-
-    let  subscCourse = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".subsc_course",
-            start: "-=240px",
-            scrub: false,
-        },
-    });
-    subscCourse
-    .to(hukidashi, { scale: 1, opacity: 1, duration: 0.5, ease: "Power4.easeOut"});
-
-    let  course_decoration_text = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".course_decoration_text",
-            start: "-=280px",
-            scrub: false,
-        },
-    });
-    course_decoration_text
-    .from(".course_decoration_text", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"})
-
-    gsap.to(".bg_orange", {
-        scrollTrigger: {
-            trigger: ".bg_orange",
-            start: "-=400px",
-            toggleClass: {
-            targets: ".bg_orange",
-            className: "active",
+    // レスポンシブ対応
+    let mm = gsap.matchMedia();
+    /* sp */
+    mm.add("(max-width: 767px)", () => {
+        /* featureセクション */
+        let speech = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".feature_headline",
+                start: "-=300px",
+                scrub: false,
             },
-            once: true,
-        },
+        });
+        speech
+        .from(".feature_head .speech", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"});
+
+        /* flowセクション */
+        let flow_upper_title = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".flow_headline",
+                start: "-=260px",
+                scrub: false,
+            },
+        });
+        flow_upper_title
+        .from(".flow_upper_title", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"}, "+=0.4");
+
+        let  flowSupport = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".flow_support",
+                start: "-=400px",
+                scrub: false,
+            },
+        });
+
+        flowSupport
+        .from(".kirakira_01", { scale: 0, opacity: 0, duration: 0.2, ease: "bounce.out"})
+        .from(".kirakira_02", { scale: 0, opacity: 0, duration: 0.2, ease: "bounce.out"}, "-=0.2");
+
+        /* priceセクション */
+        let price_upper_arraow = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".price_upper_arraow",
+                start: "-=480px",
+                scrub: false,
+                markers: true
+            },
+        });
+        price_upper_arraow
+        .from(".price_upper_arraow", { y: -10, duration: 0.5, ease: "Power4.easeOut"}, "+=0.4");
+
+        let hukidashi = CSSRulePlugin.getRule(".subsc_course::after");
+
+        let  subscCourse = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".subsc_course",
+                start: "-=440px",
+                scrub: false,
+            },
+        });
+        subscCourse
+        .to(hukidashi, { scale: 1, opacity: 1, duration: 0.5, ease: "Power4.easeOut"});
+
+        let  course_decoration_text = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".course_decoration_text",
+                start: "-=480px",
+                scrub: false,
+            },
+        });
+        course_decoration_text
+        .from(".course_decoration_text", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"})
+
+        gsap.to(".bg_orange", {
+            scrollTrigger: {
+                trigger: ".bg_orange",
+                start: "-=560px",
+                toggleClass: {
+                targets: ".bg_orange",
+                className: "active",
+                },
+                once: true,
+            },
+        });
+
+        let underline01 = CSSRulePlugin.getRule(".underline_anime_01::after");
+
+        gsap.to(underline01, {
+            scrollTrigger: {
+                trigger: ".single_course",
+                start: "-=400px",
+                toggleClass: {
+                targets: ".underline_anime_01",
+                className: "active",
+                },
+                once: true,
+            },
+        });
+
+        let underline02 = CSSRulePlugin.getRule(".underline_anime_02::after");
+
+        gsap.to(underline02, {
+            scrollTrigger: {
+                trigger: ".subsc_course",
+                start: "-=400px",
+                toggleClass: {
+                targets: ".underline_anime_02",
+                className: "active",
+                },
+                once: true,
+            },
+        });
     });
 
-    let underline01 = CSSRulePlugin.getRule(".underline_anime_01::after");
-
-    gsap.to(underline01, {
-        scrollTrigger: {
-            trigger: ".single_course",
-            start: "-=200px",
-            toggleClass: {
-            targets: ".underline_anime_01",
-            className: "active",
+    /* pc */
+    mm.add("(min-width: 768px)", () => {
+        /* featureセクション */
+        let speech = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".feature_headline",
+                start: "-=400px",
+                scrub: false,
+                markers: true
             },
-            once: true,
-        },
-    });
+        });
+        speech
+        .from(".feature_head .speech", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"});
 
-    let underline02 = CSSRulePlugin.getRule(".underline_anime_02::after");
-
-    gsap.to(underline02, {
-        scrollTrigger: {
-            trigger: ".subsc_course",
-            start: "-=200px",
-            toggleClass: {
-            targets: ".underline_anime_02",
-            className: "active",
+        /* flowセクション */
+        let flow_upper_title = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".flow_headline",
+                start: "-=400px",
+                scrub: false,
             },
-            once: true,
-        },
+        });
+        flow_upper_title
+        .from(".flow_upper_title", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"}, "+=0.4");
+
+        let  flowSupport = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".flow_support",
+                start: "-=460px",
+                scrub: false,
+            },
+        });
+
+        flowSupport
+        .from(".kirakira_01", { scale: 0, opacity: 0, duration: 0.2, ease: "bounce.out"})
+        .from(".kirakira_02", { scale: 0, opacity: 0, duration: 0.2, ease: "bounce.out"}, "-=0.2");
+
+        /* priceセクション */
+        let price_upper_arraow = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".price_upper_arraow",
+                start: "-=580px",
+                scrub: false,
+            },
+        });
+        price_upper_arraow
+        .from(".price_upper_arraow", { y: -10, duration: 0.5, ease: "Power4.easeOut"}, "+=0.4");
+
+        let hukidashi = CSSRulePlugin.getRule(".subsc_course::after");
+
+        let  subscCourse = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".subsc_course",
+                start: "-=540px",
+                scrub: false,
+            },
+        });
+        subscCourse
+        .to(hukidashi, { scale: 1, opacity: 1, duration: 0.5, ease: "Power4.easeOut"});
+
+        let  course_decoration_text = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".course_decoration_text",
+                start: "-=580px",
+                scrub: false,
+            },
+        });
+        course_decoration_text
+        .from(".course_decoration_text", { scale: 0, opacity: 0, duration: 0.5, ease: "Power4.easeOut"})
+
+        gsap.to(".bg_orange", {
+            scrollTrigger: {
+                trigger: ".bg_orange",
+                start: "-=660px",
+                toggleClass: {
+                targets: ".bg_orange",
+                className: "active",
+                },
+                once: true,
+            },
+        });
+
+        let underline01 = CSSRulePlugin.getRule(".underline_anime_01::after");
+
+        gsap.to(underline01, {
+            scrollTrigger: {
+                trigger: ".single_course",
+                start: "-=500px",
+                toggleClass: {
+                targets: ".underline_anime_01",
+                className: "active",
+                },
+                once: true,
+            },
+        });
+
+        let underline02 = CSSRulePlugin.getRule(".underline_anime_02::after");
+
+        gsap.to(underline02, {
+            scrollTrigger: {
+                trigger: ".subsc_course",
+                start: "-=500px",
+                toggleClass: {
+                targets: ".underline_anime_02",
+                className: "active",
+                },
+                once: true,
+            },
+        });
     });
 });
